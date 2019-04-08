a6d064585cd07d3e8c1a5623587a9ada41b5b89f
秦几许 <guoshaoqi1995@163.com>
2019-04-08 14:37:18 +0800

    消费者小程序

---
+++ pdjusers/app.js
@@ -0,0 +1,49 @@
+//app.js
+/**
+ * 消费者小程序功能：
+ * 1. 扫描二维码绑定微信
+ * 2. 点击关注公众号
+ * 3. 为消费者生成一个专属二维码（供店主扫描选择充值/消费）
+ * 4.
+ *    相关页面：
+ *    1.首页（index）
+ *    2.我的（mine）
+ */
+
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
