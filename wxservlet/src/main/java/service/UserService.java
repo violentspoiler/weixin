3f4a4ad7940a2040dc48ac7763696aa0f18baf47
秦几许 <guoshaoqi1995@163.com>
2019-04-15 13:56:47 +0800

    java后台

--- wxservlet/src/main/java/service/UserService.java
+++ wxservlet/src/main/java/service/UserService.java
@@ -11,16 +11,24 @@ import pojo.User;
 public class UserService {
 	    private String appid = "wx1f8fe95a40e06a47";
         private String secretKey = "12f425fe324030a17a7c02ae3c10f017";
+        private String WeChat_biz = "MzIzOTU0NTQ0MA==";//公众号id
         SentURL sentURL=new SentURL(appid, secretKey);
         public String doWhat(String code) throws NumberFormatException, SQLException {
     		// TODO 根据传输来的code，method，cash进行注册操作
-        	User user=new User(sentURL.getIdFromDB(code));
+        	String userId=sentURL.getIdFromDB(code);
+        	User user=null;
         	UserDao ud=new UserDao();
-        	if(ud.findUserByUserId(user.getUserId())==null) {
-        		ud.addUser(user);
-        		return "regist";
+        	if(ud.findUserByUserId(userId)==null) {
+        		
+        		ud.addUser(new User(userId));
+        		//注册获取公众号链接
+        		return "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz="+this.WeChat_biz+"#wechat_redirect";
+        	}else {//登录获取余额
+        		user=ud.findUserByUserId(userId);
+        		double cash=user.getCash();
+        		return cash+"";
         	}
-        	return "login";
+        	
     	}
         
 }
