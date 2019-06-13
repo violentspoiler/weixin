219681f55c42029bde1294e6d831d9e6e6ede983
guoshaoqi1995@163.com <guoshaoqi1995@163.com>
2019-06-13 17:04:11 +0800

    first

---
+++ pages/index/index.js
@@ -0,0 +1,93 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    col_count:5,
+    index_classification:[
+      {
+        lable:'lable1',
+        icon:'https://wux.cdn.cloverstd.com/logo.png'
+      },{
+        lable:'lable2',
+        icon:'https://wux.cdn.cloverstd.com/logo.png'
+      },{
+        lable:'lable3',
+        icon:'https://wux.cdn.cloverstd.com/logo.png'
+      },{
+        lable:'lable4',
+        icon:'https://wux.cdn.cloverstd.com/logo.png'
+      },{
+        lable:'lable5',
+        icon:'https://wux.cdn.cloverstd.com/logo.png'
+      },{
+        lable:'lable6',
+        icon:'https://wux.cdn.cloverstd.com/logo.png'
+      },{
+        lable:'lable7',
+        icon:'https://wux.cdn.cloverstd.com/logo.png'
+      },{
+        lable:'lable8',
+        icon:'https://wux.cdn.cloverstd.com/logo.png'
+      },{
+        lable:'lable9',
+        icon:'https://wux.cdn.cloverstd.com/logo.png'
+      },{
+        lable:'lable9',
+        icon:'https://wux.cdn.cloverstd.com/logo.png'
+      }
+    ], 
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
